7904288105ce7232a5554e60c8966561b8a0e2b1
Arun <rarnkmrbridgelabz.com>
2022-04-16 09:06:31 +0530

    Adding UC-1

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,7 +1,10 @@
 package com.bridgelabz;
 
 public class AddressBook {
+
     public static void main(String[] args) {
         System.out.println("Welcome to IO-AddressBook");
+        AddressBookContacts contact = new AddressBookContacts("Arun kumar","R","Earth","Chennai","TN","arun@gmail.com",632406,987654321);
+        System.out.println("Contact created for:"+contact.firstName +" "+contact.lastName);
     }
 }
---
+++ src/com/bridgelabz/AddressBookContacts.java
@@ -0,0 +1,96 @@
+package com.bridgelabz;
+
+public class AddressBookContacts {
+    public String firstName,lastName,address,city,state,email;
+    public  long Zip;
+    public  long phoneNo;
+
+    public AddressBookContacts(String firstName, String lastName, String address, String city, String state, String email, long zip, long phoneNo) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.email = email;
+        Zip = zip;
+        this.phoneNo = phoneNo;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getZip() {
+        return Zip;
+    }
+
+    public void setZip(long zip) {
+        Zip = zip;
+    }
+
+    public long getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(long phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    @Override
+    public String toString() {
+        return "AddressBookContacts{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", email='" + email + '\'' +
+                ", Zip=" + Zip +
+                ", phoneNo=" + phoneNo +
+                '}';
+    }
+}
